64caca13440e09ddda66aeae919f5a4a0ab30505
Eric Githinji <ericgithinjimaina@gmail.com>
2015-06-10 16:24:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost","root","3825266243","nafisika")
+$connection = mysqli_connect("localhost","root","","")
 		or die("Error ".mysqli_error($connection));
 ?>
 
